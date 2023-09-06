69b7b343ce61f202a9fe3d0622aff937c066687f
Corentin-Martin <corentin.mrtn@yahoo.com>
2023-09-06 11:07:30 +0200

    first tests for calendar

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,24 +1,9 @@
-import axios from 'axios';
-import { useState } from 'react';
+import TestCalendar from '../TestCalendar/TestCalendar';
 import './App.scss';
 
 function App() {
-  const [brands, setBrands] = useState([]);
-
-  axios.get('http://localhost:8000/api/brands')
-    .then((response) => {
-      setBrands(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
   return (
-    <div className="app">
-      <ul>
-        {brands.map((brand) => <li key={brand.id}>{brand.name}</li>)}
-      </ul>
-    </div>
+    <TestCalendar />
   );
 }
 
