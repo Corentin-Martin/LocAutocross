1a066a89b0ace445e027422b962ae137264c2c4e
Corentin-Martin <corentin.mrtn@yahoo.com>
2023-09-12 12:19:45 +0200

    first styles for rental component

--- front/src/components/Event/RentalList/RentalList.js
+++ front/src/components/Event/RentalList/RentalList.js
@@ -1,6 +1,11 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setModalCalendarIsOpen } from '../../../actions/generalCalendar';
 import './RentalList.scss';
 
 function RentalList({ rentals }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   return (
 
     (rentals.length === 0)
@@ -10,7 +15,13 @@ function RentalList({ rentals }) {
           <h3>A louer</h3>
           <ul>
             {rentals.map((rental) => (
-              <li key={rental.id}>
+              <li
+                className="RentalList-Li"
+                onClick={(() => {
+                  navigate(`location/${rental.id}`); dispatch(setModalCalendarIsOpen(false));
+                })}
+                key={rental.id}
+              >
                 {rental.vehicle.category.map((category) => (
                   ` ${category.name} /`
                 ))} {rental.vehicle.brand.name} - {rental.vehicle.model}.
--- front/src/components/Rental/Rental.js
+++ front/src/components/Rental/Rental.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import moment from 'moment/moment';
 import './Rental.scss';
 
 function Rental() {
@@ -8,13 +9,15 @@ function Rental() {
   const [rental, setRental] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  const navigate = useNavigate();
+
   const statusMatching = {
-    0: 'Brouillon',
-    1: 'Publié',
-    2: 'Interessé',
-    3: 'Reservé',
-    4: 'Validé',
-    5: 'Archivé',
+    0: ['Brouillon', '#fff'],
+    1: ['Disponible', '#00FF00'],
+    2: ['Interessé', '#FFFF00'],
+    3: ['En cours de réservation', '#ff8000'],
+    4: ['Réservation validée', '#FF0000'],
+    5: ['Archivé', '#fff'],
   };
 
   useEffect(() => {
@@ -31,7 +34,54 @@ function Rental() {
   return (
     <div className="Rental">
       {isLoading ? <p>Chargement...</p> : (
-        <h1 className="test">{rental.id} {statusMatching[rental.status]}</h1>
+        <div className="Rental-Box">
+          <div onClick={() => (navigate('/'))} className="Rental-Box-Event">
+            <h2>{rental.event.title}</h2>
+            <p>Date de début : {moment(rental.event.start).format('DD/MM/YYYY')}</p>
+            <p>Date de fin : {moment(rental.event.start).format('DD/MM/YYYY')}</p>
+            {rental.event.championship !== null
+              && (
+              <p>Championnat : {`${rental.event.championship.alias} `}
+                - {rental.event.championship.federation.alias}
+              </p>
+              )}
+          </div>
+          <h2 className="Rental-Box-Title">{`${rental.vehicle.brand.name} - `}
+            <span style={{ backgroundColor: statusMatching[rental.status][1] }}>
+              {statusMatching[rental.status][0]}
+            </span>
+          </h2>
+          <img className="Rental-Box-Image" src={rental.vehicle.picture ?? 'https://cdn-icons-png.flaticon.com/512/3272/3272412.png'} alt="vehicule" />
+          <div className="Rental-Box-Infos">
+            <div className="Rental-Box-Infos-Element">
+              <h3 className="Rental-Box-Infos-Element-Title">La location</h3>
+              <div className="Rental-Box-Infos-Element-Sub">
+
+                <p>Tarif : {rental.price ? `${rental.price}€` : 'Non renseigné'}</p>
+                <p>Loueur : {rental.ownerUser.pseudo}</p>
+                <p>{rental.description ?? 'Aucunes informations supplémentaires'}</p>
+              </div>
+            </div>
+            <div className="Rental-Box-Infos-Element">
+              <h3 className="Rental-Box-Infos-Element-Title">Le véhicule</h3>
+              <div className="Rental-Box-Infos-Element-Sub">
+                <p> Catégorie{rental.vehicle.category.length > 1 ? 's : ' : ' : '}
+                  {rental.vehicle.category.map((category, index) => (
+                    (index === rental.vehicle.category.length - 1)
+                      ? `${category.name}`
+                      : `${category.name} / `
+                  ))}
+                </p>
+                <p>Modèle : {rental.vehicle.model ?? 'Non renseigné'}</p>
+                <p>Année : {moment(rental.vehicle.year).format('YYYY')}</p>
+                <p>Moteur : {rental.vehicle.engine}</p>
+                <p>Amortisseur : {rental.vehicle.shocks ?? 'Non renseigné'}</p>
+                <p>Infos : {rental.vehicle.description ?? '/'}</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
       )}
     </div>
   );
