0cfd6321dd523f0eecb23f69a97780e34b7ce749
Corentin Martin <corentin.mrtn@yahoo.com>
2023-09-18 16:30:11 +0200

    prepare rental page

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -2,11 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
-import Rental from '../Rental/Rental';
 import Chat from '../Chat/Chat';
 import './App.scss';
 import Skeleton from '../Skeleton/Skeleton';
 import Homepage from '../../pages/Homepage/Homepage';
+import Rental from '../../pages/Rental/Rental';
 import Calendar from '../../pages/Calendar/Calendar';
 
 function App() {
@@ -22,7 +22,11 @@ function App() {
           path="/calendrier"
           element={(<Skeleton page={<Calendar />} />)}
         />
-        <Route path="location/:rentalId" element={<Rental />} />
+        <Route
+          path="/location/:rentalId"
+          element={(<Skeleton page={<Rental />} />)}
+        />
+
         <Route path="login" element={<Login />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="conversation/:id" element={<Chat />} />
---
+++ front/src/pages/Rental/Rental.js
@@ -0,0 +1,10 @@
+import './Rental.scss';
+import RentalComponent from '../../components/RentalComponent/RentalComponent';
+
+function Rental() {
+  return (
+    <RentalComponent />
+  );
+}
+
+export default Rental;
