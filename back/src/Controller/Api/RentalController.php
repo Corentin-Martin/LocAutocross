d4baade299539de7e7611a9bcdddb448c1ec01d3
Corentin Martin <corentin.mrtn@yahoo.com>
2023-09-26 16:06:52 +0200

    find rentals with only 1 or 2 status

--- back/src/Controller/Api/RentalController.php
+++ back/src/Controller/Api/RentalController.php
@@ -24,7 +24,7 @@ class RentalController extends AbstractController
     public function browse(Request $request, RentalRepository $rentalRepository): JsonResponse
     {
         if (!is_null($request->query->get('last'))) {
-            $rentals = $rentalRepository->findBy([],['createdAt' => 'DESC'],4);
+            $rentals = $rentalRepository->findLastPublished();
             return $this->json($rentals, Response::HTTP_OK, [], ["groups" => ["rentals"]]);
         }
 
