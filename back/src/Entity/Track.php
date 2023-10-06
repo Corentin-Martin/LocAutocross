993f86c139f65d4d4f7bdf741366ca7cf21393a3
Corentin Martin <corentin.mrtn@yahoo.com>
2023-10-06 10:59:45 +0200

    migration to add lat et lng for track + serialize them

--- back/src/Entity/Track.php
+++ back/src/Entity/Track.php
@@ -89,6 +89,20 @@ class Track
      */
     private $events;
 
+    /**
+     * @ORM\Column(type="float")
+     * @Groups({"tracks"})
+     * @Groups({"track"})
+     */
+    private $latitude;
+
+    /**
+     * @ORM\Column(type="float")
+     * @Groups({"tracks"})
+     * @Groups({"track"})
+     */
+    private $longitude;
+
     public function __construct()
     {
         $this->events = new ArrayCollection();
@@ -219,5 +233,29 @@ class Track
     public function onPreUpdate()
     {
         $this->updatedAt = new \DateTime("now");
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): self
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
     }
 }
