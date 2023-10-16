bda4e3992978959aede17b8d4f243883ceb64449
Corentin Martin <corentin.mrtn@yahoo.com>
2023-10-16 14:25:04 +0200

    delete method for user

--- back/src/Controller/Api/UserController.php
+++ back/src/Controller/Api/UserController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Api;
 
 use App\Entity\User;
+use App\Repository\CommentRepository;
+use App\Repository\ConversationRepository;
+use App\Repository\EventRepository;
+use App\Repository\MessageRepository;
+use App\Repository\RentalRepository;
 use App\Repository\UserRepository;
+use App\Repository\VehicleRepository;
 use App\Services\EmailSender;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -69,6 +75,60 @@ class UserController extends AbstractController
         $userRepository->add($user, true);
 
         return $this->json($user, Response::HTTP_OK, [], ["groups" => ["user"]]);
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete(User $user, UserRepository $userRepository, MessageRepository $messageRepository, RentalRepository $rentalRepository, CommentRepository $commentRepository, ConversationRepository $conversationRepository, VehicleRepository $vehicleRepository, EventRepository $eventRepository): JsonResponse
+    {
+
+        if ($user !== $this->getUser()) {
+            return $this->json(["message" => "Vous ne pouvez pas supprimer cet utilisateur"], Response::HTTP_FORBIDDEN);
+        }
+
+        $removedUser = $userRepository->findOneBy(["id" => 60]);
+
+        foreach ($user->getMessages() as $message) {
+            $message->setUser($removedUser);
+            $messageRepository->add($message, true);
+        }
+
+        foreach ($user->getConversations() as $conversation) {
+            $conversation->setInterestedUser($removedUser);
+            $conversationRepository->add($conversation, true);
+        }
+
+        foreach ($user->getEvents() as $event) {
+            $event->setAssociatedUser($removedUser);
+            $eventRepository->add($event, true);
+        }
+
+        foreach ($user->getVehicles() as $vehicle) {
+            $vehicle->setOwnerUser($removedUser);
+            $vehicleRepository->add($vehicle, true);
+        }
+
+        foreach ($user->getComments() as $comment) {
+            $comment->setAssociatedUser($removedUser);
+            $commentRepository->add($comment, true);
+        }
+
+        foreach ($user->getReservations() as $rental) {
+            $rental->setTenantUser($removedUser);
+            $rental->setStatus(1);
+            $rentalRepository->add($rental, true);
+        }
+
+        foreach($user->getPropositions() as $rental) {
+            $rental->setOwnerUser($removedUser);
+            $rental->setStatus(5);
+            $rentalRepository->add($rental, true);
+        }
+        
+        $userRepository->remove($user, true);
+
+        return $this->json(["message" => "L'utilisateur a été supprimé"], Response::HTTP_OK);
     }
 }
 
